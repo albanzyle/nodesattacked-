85748d36bb3e9b0a767a116ffa3cef8c3b5de467
albanzyle <albanzyle>
2023-11-19 20:04:10 +0100

    24 random attaclks for pij. Attacking n nodes from which 2/3 are controllers common between PF and LF

--- kot.py
+++ kot.py
@@ -1,15 +1,34 @@
-import numpy as np
+import random
 
-# Example usage
-C = [1, 2, 3]  # Set of controllers
-V = [1, 2, 3, 4]  # Set of vertices
-p = {1: {1: 0.1, 2: 0.2, 3: 0.3, 4: 0.4},
-     2: {1: 0.4, 2: 0.3, 3: 0.2, 4: 0.1},
-     3: {1: 0.2, 2: 0.1, 3: 0.4, 4: 0.3}}  # Probability matrix
-alpha = np.zeros((len(C), len(V)))
+# Dummy data
+sa_nodes = list(range(1, 10))  # Assume you have 9 nodes
+controllers = [1, 3, 5]  # Controllers vector
+controllers2 = [2, 3, 5]  # Indexes for controllers2
 
-# Populate alpha matrix
-for i, controller in enumerate(C):
-    for j, vertex in enumerate(V):
-        alpha[i, j] = np.log(1 - p[controller][vertex])
-print(alpha)
+# Print the dummy data
+print("sa_nodes:", sa_nodes)
+print("controllers:", controllers)
+print("indexes:", controllers2)
+
+# controllers2 = [indexes[i] for i in range(len(controllers))]
+
+# Get common nodes between controllers and controllers2
+common_nodes = list(set(controllers) & set(controllers2))
+print("common_nodes:", common_nodes)
+
+sa_attack_nodes_500_random = {}
+available_nodes = list(set(sa_nodes) - set(controllers) - set(controllers2))
+print("available_nodes:", available_nodes)
+
+for i in range(1, 5):  # Assume you have 5 iterations
+    random_size = 4
+    # Exclude nodes from controllers and controllers2 in the random sample
+    random_nodes = set(random.sample(available_nodes, random_size - 2))
+
+    # Add two nodes from the common_nodes list
+    random_nodes.update(random.sample(common_nodes, 2))
+
+    sa_attack_nodes_500_random[i] = random_nodes
+
+# Print the result
+print("sa_attack_nodes_500_random:", sa_attack_nodes_500_random)
